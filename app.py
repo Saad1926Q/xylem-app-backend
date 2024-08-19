c7a9a7c49933474bf92b11e9c1f68509492696d4
Saad1926Q <saad.ahmed1926q@gmail.com>
2024-08-19 17:21:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask,render_template,request,flash, request, redirect, url_for,jsonify
+from werkzeug.utils import secure_filename
+import os
+
+from models import Model
+
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app=Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY']='3d6f45a5fc12445dbac2f59c3b6c7cb1'
+
+model=Model()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload',methods=["POST"])
+def upload():
+    if 'file' not in request.files:
+        return jsonify({"error":"No file part"})
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error":"No selected file"})
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return process_data(file_path)
+    elif file and not allowed_file(file.filename):
+        return jsonify({"error":"Incorrect Format"})
+    
+
+def process_data(file_path):
+    model.image_to_feature_vector(image_path=file_path)
+    pred=model.image_prediction(image=model.image)
+    return jsonify({"prediction":pred})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    #In the above you can add ip address with the help of host='<ip_address>', port=8080
+
+
+
